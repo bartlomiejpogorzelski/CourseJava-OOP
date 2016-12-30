8b0b8d556c0383f0f7601498f58fb444dac5247f
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2016-12-30 14:43:30 +0100

    Add some definition

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -6,8 +6,13 @@ public class CourseJavaOOP {
 
     public static void main(String[] args) {
     /*
-        
-    */
+     Objects - containers that stores variables and functions thematically
+                connected to each other for easier future usage
+     Classes - forms for creating instances (copies) of objects   
+    
+     Properties - features (part of something)
+     Methods - functions
+       */
     }
     
 }
