ae7d195dd9a5ed57a0fb969c5272fd053373c9ab
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-02 18:31:26 +0100

    Creating the objects using constructor

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -14,10 +14,13 @@ public class CourseJavaOOP {
        */
     
     Point p= new Point(33,88);
-    
+    Point p2= new Point(334,848);
 
         System.out.println(p.x);
         System.out.println(p.y);
+        
+        System.out.println(p2.x);
+        System.out.println(p2.y);
     }
     
 }
