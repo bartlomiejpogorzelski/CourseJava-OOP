52197f86379027392317c7ddca080947b0d7a581
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-03 19:35:33 +0100

    Static properties and methods

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -6,8 +6,11 @@ public class CourseJavaOOP {
     public static void main(String[] args) {
         
       
-        double i = Math.add(13, 43);
-        System.out.println(i);
+//        double i = Math.add(13, 43);
+//        System.out.println(i);
+    Customer a = new Customer("adam");
+    Customer b = new Customer("Bartek");
+    Customer c = new Customer("Czeslaw");
     }
 }
 
@@ -16,5 +19,16 @@ class Math
     static double add(double a, double b)
     {
     return a+b;
+    }
+}
+
+class Customer
+{
+    String name;
+    int id; 
+    
+    Customer(String name)
+    {
+    this.name=name;
     }
 }
