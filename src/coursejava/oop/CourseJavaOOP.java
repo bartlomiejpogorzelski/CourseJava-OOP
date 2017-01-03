9a1632278c0e166d7d1f3cd786f2dc56e02a76eb
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-03 16:09:10 +0100

    Static properties and methods
    Add word "static" in method.

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -5,15 +5,15 @@ public class CourseJavaOOP {
 
     public static void main(String[] args) {
         
-        Math test = new Math();
-        double i = test.add(13, 43);
+      
+        double i = Math.add(13, 43);
         System.out.println(i);
     }
 }
 
 class Math
 {
-    double add(double a, double b)
+    static double add(double a, double b)
     {
     return a+b;
     }
