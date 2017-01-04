7f279dfa5cb2653b4e13bc4a377da71177d42b57
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-04 11:11:01 +0100

    Packages
    Use import package and create object

--- src/newpackage/NewClass.java
+++ src/newpackage/NewClass.java
@@ -1,5 +1,9 @@
 package newpackage;
 
+import coursejava.oop.CourseJavaOOP;
 public class NewClass {
+    CourseJavaOOP a = new CourseJavaOOP();
+    // or:
+    //coursejava.oop.CourseJavaOOP a = new coursejava.oop.CourseJavaOOP();
     
 }
