f976e9aa9ab67fc85809e4311f2e2ce81770aedc
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-04 13:30:50 +0100

    Packages
    Class Test work in the class in the same packages

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -4,7 +4,8 @@ package coursejava.oop;
 public class CourseJavaOOP {
 
     public static void main(String[] args) {
-        
-
+       //"Test" works in the same package "coursejava.opp" 
+        Test.a = 5;
+        System.out.println(Test.a);
     }
 }
