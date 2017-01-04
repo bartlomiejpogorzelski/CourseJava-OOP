e872fd1d411ee2f3ce6dd3537ead9af63fb991fc
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-04 13:54:18 +0100

    Packages
    Use class from the other package

--- src/newpackage/NewClass.java
+++ src/newpackage/NewClass.java
@@ -1,9 +1,12 @@
 package newpackage;
 
 import coursejava.oop.CourseJavaOOP;
+import coursejava.oop.Test;
 public class NewClass {
-    CourseJavaOOP a = new CourseJavaOOP();
+    //CourseJavaOOP a = new CourseJavaOOP();
     // or:
     //coursejava.oop.CourseJavaOOP a = new coursejava.oop.CourseJavaOOP();
-    
+   public static void main(String[] args){
+    Test.a=5;
+}
 }
