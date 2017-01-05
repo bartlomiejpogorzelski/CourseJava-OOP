fdd5957d85d0f5f14368c95af2033dfa58325173
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-05 21:25:10 +0100

    Passing refernces
    Initiation

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -5,30 +5,6 @@ public class CourseJavaOOP {
 
     public static void main(String[] args) {
     
-        int x = 50;
-        int y = x;
-        
-        y= 14;
-        
-        System.out.println(x);
-        // y is only a copy, so we have 50
-        
-        //here we assign adress (t) not value.
-        Test t =new Test(); // refernce variable beacuse we refer to the place in the memory
-        Test t2 = t;
-        t2.a=79;
-        System.out.println(t.a);//79
-        
-        
-        String name ="Arkadiusz";
-        String name2 = name;
-        
-        name2= "wiola";
-        System.out.println(name); // give Arkadiusz
-    }
+       
 }
-
-class Test
-{
-    int a = 80;
 }
