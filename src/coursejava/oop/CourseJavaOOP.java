d5a63faf5913c47593a350aceb7aab6c207c0495
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-05 16:23:58 +0100

    References

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -5,4 +5,12 @@ public class CourseJavaOOP {
 
     public static void main(String[] args) {
     
+        int x = 50;
+        int y = x;
+        
+        y= 14;
+        
+        System.out.println(x);
+        // y is only a copy, so we have 50
+    }
 }
