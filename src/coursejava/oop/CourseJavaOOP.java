7e84157d81c4f0e5e5971b18d46b5d408cac8ca5
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-08 13:21:06 +0100

    How to compare string properly
    Initiation

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -4,34 +4,6 @@ package coursejava.oop;
 public class CourseJavaOOP {
 
     public static void main(String[] args) {
-        int a =10;
-       
-        Test test = new Test();
-        
-        test.changeValue(a);
-        System.out.println(a);
-        
-        Foo foo=new Foo();
-        test.changeFoo(foo); //Adress
-        System.out.println(foo.x);
-}
-}
-
-class Test
-{
-    void changeValue(int var) // int var = 10
-   {
-       var= var + 5; 
-  
-   }
     
-    void changeFoo(int var) // Foo var = Adress
-    {
-        var.x = var.x +15;
     }
-}
-
-class Foo
-{
-    int x= 11;
 }
