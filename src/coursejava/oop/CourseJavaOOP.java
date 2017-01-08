4a47103bfacb197ffcb3ea41131a519fe0e33cf4
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-08 13:23:13 +0100

    How to compare String properly
    example

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -4,6 +4,10 @@ package coursejava.oop;
 public class CourseJavaOOP {
 
     public static void main(String[] args) {
+    String name= "Arkadiusz";
+    String name2= "Arkadiusz";
     
+    if (name == name2)
+            System.out.println("Are equal");
     }
 }
