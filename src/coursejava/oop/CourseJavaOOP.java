946f4328ff8b3391ab37ebb8b023fd26a7ddfe78
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-10 11:06:35 +0100

    Inheritance
    Inherite class from other

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -1,13 +1,13 @@
 package coursejava.oop;
 
 //Packages
-
+import coursejava.monsters.Monster;
 
 
 public class CourseJavaOOP {
 
     public static void main(String[] args) {
     // MONSTERS - ZOMBIES - SKELETONS - hit points, attack, speed
-       
+       Monster monster = new Monster();
     }
 }
