e8e3365d6e2cc4a2cdcc8adb8b399862cbf1d335
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-10 11:43:51 +0100

    Inheritance
    involve

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -8,8 +8,11 @@ public class CourseJavaOOP {
 
     public static void main(String[] args) {
     // MONSTERS - ZOMBIES - SKELETONS - hit points, attack, speed
-       Monster monster = new Monster();
+       Monster monster = new Monster(100,10);
        
        monster.Attack();
+       
+        System.out.println(monster.hitPoints);
+        System.out.println(monster.speed);
     }
 }
