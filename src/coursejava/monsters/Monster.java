645c77445f2179c79d45996afd64f2b229bc10ab
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-10 11:34:33 +0100

    Inheritance
    add constructors

--- src/coursejava/monsters/Monster.java
+++ src/coursejava/monsters/Monster.java
@@ -6,6 +6,16 @@ public class Monster {
     public double hitPoints;
     public double speed;
     
+    public Monster()
+    {
+        
+    }
+    
+    public Monster(double hitPoints, double speed)
+    {
+        this.hitPoints=hitPoints;
+        this.speed=speed;
+    }
     public void Attack()
     {
         System.out.println("I'm attacking from monster class"); 
