9c0061d433bec508d9177553b35c5148ea25c27d
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-01-10 10:46:22 +0100

    Inheritance
    Add new classes and pacage

--- src/coursejava/oop/CourseJavaOOP.java
+++ src/coursejava/oop/CourseJavaOOP.java
@@ -1,10 +1,13 @@
 package coursejava.oop;
 
 //Packages
+
+
+
 public class CourseJavaOOP {
 
     public static void main(String[] args) {
     // MONSTERS - ZOMBIES - SKELETONS - hit points, attack, speed
-
+       
     }
 }
